2eec9241f3bebad2389a53c55163511ce3f7d15d
this.username <52316802+LokendraSinghRathore@users.noreply.github.com>
2021-05-10 15:44:52 +0530

    Update quickHeathCheck.sh

--- quickHeathCheck.sh
+++ quickHeathCheck.sh
@@ -3,6 +3,8 @@
 #
 red='\033[0;31m'
 green='\033[0;32m'
+#value1 is url without containing "https://"
+#value2 is Website name 
 while IFS=" " read -r value1 value2 remainder
 do
 	status=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "https://$value1")
