b70bc14443f243ba8431d66ce8be06e80de8219a
this.username <52316802+LokendraSinghRathore@users.noreply.github.com>
2021-05-10 16:14:03 +0530

    Update frequentlyWallpaperChanger.sh

--- frequentlyWallpaperChanger.sh
+++ frequentlyWallpaperChanger.sh
@@ -1,1 +1,16 @@
 gsettings set org.gnome.desktop.background picture-uri "https://source.unsplash.com/random/1366x768"
+
+
+#to utalise local images:
+
+var=1;
+while [ 1 ]
+do 
+gsettings set org.gnome.desktop.background picture-uri "/root/Videos/index.html.$var";
+var=$((var+1));
+if [ $var == 636 ]; 
+then 
+var=0;
+fi; 
+sleep 7;
+done
